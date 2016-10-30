c0145750544bfadae196ce1668d83514d4a6584d
nick0916 <nickliao2013@gmail.com>
2016-10-30 12:06:54 +0800

    show picture function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {  });
+
+
+       res.render('index', {});
+
+  
+});
+
+router.get('/:date', function(req, res, next) {
+   fs.readdir('./public/img/' + req.params.date, function(err, files) {
+       if (err) throw err;
+       
+      var file = [];
+      for(var i in files){
+         if (files[i] == ".DS_Store") continue;
+         file.push(files[i]);
+      }
+
+       res.render('picture', {'files': files, dir: req.params.date });
+   });
+  
 });
 
 module.exports = router;
